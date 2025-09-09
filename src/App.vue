00de41f2e6ebe85eda795dc02e54602533c6b7eb
ByAlonso <alonso.rapado@hotmail.com>
2025-09-09 19:01:56 +0200

    First commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+function clickMe() {
+  console.log("Hola")
+}
+</script>
+
+<template>
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+  <h1 class="text-4xl font-black">Hola</h1>
+  <button @click="clickMe()">Click me</button>
+</template>
+
+<style scoped></style>
